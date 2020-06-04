074bd750a006469a6b684010241b650aa7c071ef
Michaeldremy <michaeldremy@gmail.com>
2020-06-03 21:25:53 -0700

    New folder structure

---
+++ Fundamentals/PythagoreanThoerem/PythagoreanThoerem.java
@@ -0,0 +1,6 @@
+public class PythagoreanThoerem {
+    public double calculateHypotenuse(int legA, int legB) {
+        double c = Math.sqrt(legA^2 + legB^2);
+        return c;
+    }
+}
---
+++ Fundamentals/PythagoreanThoerem/PythagoreanThoeremTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanThoeremTest {
+    public static void main(String[] args) {
+        PythagoreanThoerem iD = new PythagoreanThoerem();
+        double newTriangle = iD.calculateHypotenuse(2, 2);
+        System.out.println(newTriangle);
+    }
+}
